6070f7e9ceedaa266c080588168cde47ca80411a
Marco Vidal García <marcovidal67@gmail.com>
2026-06-22 18:29:08 +0200

    feat: add DRF, corsheaders, and /api/hello/ endpoint

--- hello/views.py
+++ hello/views.py
@@ -1,4 +1,10 @@
 from django.http import HttpResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 def index(request):
     return HttpResponse("Hello, world!")
+
+@api_view(["GET"])
+def hello_api(request):
+    return Response({"message": "Hello from Django!"})
